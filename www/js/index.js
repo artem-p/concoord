baceb8250025a65e7c911be6a4803dd2c6ba50a1
Artem Pugachev <artempugachev@iram.com>
2014-11-26 17:40:57 +0300

    Работает кнопка очистить

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,17 @@
 // Wait for device API libraries to load
 //
+var btnClear = $('#btnClear');
+var tvGeoLatDeg = $('#geoLatDeg');
+var tvGeoLatMin = $('#geoLatMin');
+var tvGeolatSec = $('#geoLatSec');
+var tvGeoLonDeg = $('#geoLonDeg');
+var tvGeoLonMin = $('#geoLonMin');
+var tvGeoLonSec = $('#geoLonSec');
+var tvDecLat = $('#decLat');
+var tvDecLon = $('#decLon');
+
 function onLoad() {
+    btnClear.click(clearFields);
     document.addEventListener("deviceready", onDeviceReady, false);
 }
 
@@ -8,5 +19,17 @@ function onLoad() {
 //
 function onDeviceReady() {
 
-    alert('ready');
+    //alert('ready');
+}
+
+function clearFields() {
+    //  Сбрасываем все поля
+    tvGeoLatDeg.val('');
+    tvGeoLatMin.val('');
+    tvGeolatSec.val('');
+    tvGeoLonDeg.val('');
+    tvGeoLonMin.val('');
+    tvGeoLonSec.val('');
+    tvDecLat.val('');
+    tvDecLon.val('');
 }
