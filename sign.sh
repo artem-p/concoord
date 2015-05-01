783322904c6eed1c83d31a5957fe93867a29e8cb
artem-p <artempugachev1@gmail.com>
2015-05-01 12:54:31 +0300

    Обновили скрипт подписи

---
+++ sign.sh
@@ -0,0 +1,3 @@
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../keystore/concoord.keystore platforms/android/ant-build/CordovaApp-release-unsigned.apk concoord 
+rm platforms/android/ant-build/concoord-release.apk
+zipalign -v 4 platforms/android/ant-build/CordovaApp-release-unsigned.apk platforms/android/ant-build/concoord-release.apk
